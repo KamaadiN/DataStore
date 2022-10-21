9d802b7ed32f8f7264445c5664c7cdf72b688ece
KamaadiN <115966181+KamaadiN@users.noreply.github.com>
2022-10-21 10:02:16 -0400

    Update Whitelist.lua

--- Whitelist.lua
+++ Whitelist.lua
@@ -1,1 +1,17 @@
-
+return {
+    3188512202, -- Sacolinha
+    2038778847, -- Escuridão
+    3067509017, -- WesleyPC
+    3332872267, -- Lirous
+    2504255348, -- DouglasNFT
+    2771766988, -- Slow
+    127184704, -- Senju
+    3568641694, -- LhyMoDs
+    2238617765, -- TheFox
+    223770715, -- Jaca
+    2427591310, -- Ninja
+    113452795, -- Mudinho
+    1934778943, -- Shonen
+    1022210737, -- Luipy
+    1340755305 -- Mazter
+}
